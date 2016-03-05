94ad6b1ad3451ef95f6b6ef941fb14313ecfbc81
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-03-06 03:22:19 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -5,12 +5,12 @@ OLD="$(date +"%d-%m-%Y" -d '-7 day')"
 # create directory
 mkdir -p "$BACKUP/sql/$NOW/"
 # all databases
-mysqldump -u root --host=$MYSQL_PORT_3306_TCP_ADDR --protocol=$MYSQL_PORT_3306_TCP_PROTO --password=$MPASS --all-databases --single-transaction > "$BACKUP/sql/$NOW/all_databases.sql"
+mysqldump -u root --host=$HOST --protocol=$PROTO --password=$PASS --all-databases --single-transaction > "$BACKUP/sql/$NOW/all_databases.sql"
 # backup each base of the database
-DBS="$(mysql -u $MUSER --host=$MYSQL_PORT_3306_TCP_ADDR --protocol=$MYSQL_PORT_3306_TCP_PROTO -p$MPASS -Bse 'show databases')"
+DBS="$(mysql -u $MUSER --host=$HOST --protocol=$PROTO -p$PASS -Bse 'show databases')"
 for db in $DBS
 do
-	mysqldump -u root --host=$MYSQL_PORT_3306_TCP_ADDR --protocol=$MYSQL_PORT_3306_TCP_PROTO --password=$MPASS $db --single-transaction > "$BACKUP/sql/$NOW/$db.sql"
+	mysqldump -u root --host=$HOST --protocol=$PROTO --password=$PASS $db --single-transaction > "$BACKUP/sql/$NOW/$db.sql"
 done
 # delete old sql
 rm -r $BACKUP/sql/$OLD*

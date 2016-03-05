55b3fa4ffa2d1390f4951dccb03be1530ff1a82f
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-03-06 03:33:12 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-BACKUP=/backup
+BACKUP=${DIR:-/backup}
+ROOTUSER=${ADMIN:-root}
 NOW="$(date +"%d-%m-%Y_%H%M")"
 OLD="$(date +"%d-%m-%Y" -d '-7 day')"
 # create directory
 mkdir -p "$BACKUP/sql/$NOW/"
 # all databases
-mysqldump -u root --host=$HOST --protocol=$PROTO --password=$PASS --all-databases --single-transaction > "$BACKUP/sql/$NOW/all_databases.sql"
+mysqldump -u $ROOTUSER --host=$HOST --protocol=$PROTO --password=$PASS --all-databases --single-transaction > "$BACKUP/sql/$NOW/all_databases.sql"
 # backup each base of the database
 DBS="$(mysql -u $MUSER --host=$HOST --protocol=$PROTO -p$PASS -Bse 'show databases')"
 for db in $DBS
 do
-	mysqldump -u root --host=$HOST --protocol=$PROTO --password=$PASS $db --single-transaction > "$BACKUP/sql/$NOW/$db.sql"
+	mysqldump -u $ROOTUSER --host=$HOST --protocol=$PROTO --password=$PASS $db --single-transaction > "$BACKUP/sql/$NOW/$db.sql"
 done
 # delete old sql
 rm -r $BACKUP/sql/$OLD*
